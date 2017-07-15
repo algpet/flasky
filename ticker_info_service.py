ec209c3b13387d3919eaafc2c89ae42451ba924a
algpet <litovec@gmail.com>
2017-07-15 19:04:21 +0300

    update

--- ticker_info_service.py
+++ ticker_info_service.py
@@ -1,14 +1,18 @@
 
 class TickerInfoService():
     def __init__(self,source_file):
+        self.UNKNOWN_TICKER = {"name": "unknown"}
         self.data = {}
         file = open(source_file)
         for line in file:
             info = line.split(",")
-            self.data[info[0].capitalize()] = {"name":info[1] }
+            self.data[info[0].upper()] = {"name":info[1] }
 
     def get_ticker_data(self,ticker):
-        return self.data[ticker.capitalize()]
+        if ticker in self.data:
+            return self.data[ticker.upper()]
+        else:
+            return self.UNKNOWN_TICKER
 
     def get_tickers_data(self,tickers):
         data = {}
