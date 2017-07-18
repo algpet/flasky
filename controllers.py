c123e7eb228d04b956987c0fe2add8ec00ab9016
algpet <litovec@gmail.com>
2017-07-18 22:56:47 +0300

    update

--- controllers.py
+++ controllers.py
@@ -5,10 +5,12 @@ from flask import render_template
 from stock_rate_service import TickerRateService
 from ticker_info_service import TickerInfoService
 from params_service import ParameterService
+from ticket_analysis_service import  TicketAnalysisService
 
 tickerRateService = TickerRateService('google')
 tickerNameService = TickerInfoService('secwiki_tickers.csv')
 parameterService = ParameterService(10)
+ticketAnalysisService = TicketAnalysisService()
 app = Flask(__name__)
 
 
@@ -22,6 +24,7 @@ def request_form():
         tickers, from_date, till_date = parameterService.process_params(request)
         if not ( tickers is None or from_date is None or till_date is None):
             ticker_data = tickerRateService.get_rates(tickers, from_date, till_date)
+            ticker_data = ticketAnalysisService.analyze_dataframes(ticker_data)
             ticker_headers = tickerNameService.get_tickers_data(tickers)
             tickers = ",".join(tickers)
 
