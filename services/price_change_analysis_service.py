4a4738c48e057567e8bebe24958bc07375e79495
algpet <litovec@gmail.com>
2017-08-13 15:49:18 +0300

    iter5 - volatility instead of stdev

--- services/price_change_analysis_service.py
+++ services/price_change_analysis_service.py
@@ -35,7 +35,104 @@ class PriceChangeAnalysisService:
         }
 
 
+    # v2
+    def simmulate_price_change(self,df):
+        import glob
+        import os
+
+        files = glob.glob('static/img/*.png')
+        for filename in files:
+            os.unlink(filename)
+        filename1 = "static/img/graph" + str(random.randint(100000000, 999999999)) + ".png"
+        filename2 = "static/img/graph" + str(random.randint(100000000, 999999999)) + ".png"
+
+        last_close = df.iloc[0]['Close']
+        year_df = df[0:251]["Close"]
+        stdev = year_df.std()
+
+
+        price = last_close
+        volatilities = self.volatilityAnalysisService.calculate_volatility(df)
+        three_week_volatility = volatilities["3week"]
+        multi = math.sqrt(math.pi / 2)
+
+        graph_xlab = [-1.5]
+        graph_x = [0]
+        graph_y = [price]
+        for iter in range(16):
+            expected_3week_shift = price * three_week_volatility / 100
+            print("exp 3w shift" , expected_3week_shift)
+            devi = expected_3week_shift * multi
+
+            price = sct.norm.ppf(random.random(), price, devi)
+            graph_y.append(round(price,2))
+            graph_x.append(3 * (iter + 1))
+            graph_xlab.append(3 * (iter + 0.5))
+
+        plt.clf()
+        plt.plot()
+        plt.xlabel('weeks')
+        plt.ylabel('price')
+        plt.plot(graph_x, graph_y, color="#FF0000", markersize=4, marker="o")
+
+        plt.xticks(graph_x)
+
+        font = {'size': 7}
+        ylim = plt.ylim()
+        xlim = plt.xlim()
+        axes = plt.gca()
+        axes.set_ylim([ylim[0] - 5, ylim[1] + 5])
+        axes.set_xlim([xlim[0] - 1.5, xlim[1]])
+        bot = plt.ylim()[0] * 1.02
+
+        colors = ["green", "blue"]
+        for i in range(len(graph_x)):
+            plt.text(graph_xlab[i], bot, str(graph_y[i]), fontdict=font, color=colors[i % 2])
+        plt.savefig(filename1)
+
+
+        ####################################################################################
+        expected_3week_shift = price * three_week_volatility / 100
+        devi = expected_3week_shift * multi
+        final_prices = []
+
+        for fp in range(100):
+            final_price = sct.norm.ppf(random.random(), price, devi)
+            final_prices.append(round(final_price, 2))
+
+        stdev_archetypes = {-3: 0, -2: 0, -1: 0, 0: 0, 1: 0, 2: 0, 3: 0}
+        for price in final_prices:
+            ac = (price - last_close) / stdev
+            ac = math.trunc(ac)
+            if ac < -4:
+                ac = -4
+            if ac > 3:
+                ac = 3
+            if ac not in stdev_archetypes:
+                stdev_archetypes[ac] = 0
+            stdev_archetypes[ac] += 1
+
+        keys = stdev_archetypes.keys()
+        keys2 = []
+        for key in keys:
+            keys2.append(key + 0.25)
+
+        plt.clf()
+        plt.plot()
+        plt.xlabel('stdevs away from avg')
+        plt.ylabel('count of simmulations')
+        plt.bar(keys2, stdev_archetypes.values(), 0.5, color='r')
+        plt.savefig(filename2)
+
+        return filename1, filename2, final_prices
+
+
+
+
+
     #just quick and dirty verion ...
+    """
+    # old version
     def simmulate_price_change(self,df):
 
         import glob
@@ -127,6 +224,6 @@ class PriceChangeAnalysisService:
 
 
         return filename1,filename2,final_prices
-
+    """
 
 
--- services/volatility_analysis_service.py
+++ services/volatility_analysis_service.py
@@ -6,6 +6,7 @@ class VolatilityAnalysisService:
         self.TRADE_DAYS_IN_YEAR = 250
         self.SQUARE_ROOT_TRADE_DAYS_IN_YEAR = math.sqrt(254)
         self.SQUARE_ROOT_OF_TRADE_DAYS_IN_MONTH = math.sqrt(254 / 12)
+        self.SQUARE_ROOT_OF_TRADE_DAYS_IN_3WEEKS = math.sqrt(254 / (365/21))
 
     def calculate_volatility(self,df):
         df = df[1:]
@@ -15,5 +16,6 @@ class VolatilityAnalysisService:
         year_volatility = std_dev * self.SQUARE_ROOT_TRADE_DAYS_IN_YEAR
         day_volatility = year_volatility / self.SQUARE_ROOT_TRADE_DAYS_IN_YEAR
         month_volatility = year_volatility / self.SQUARE_ROOT_OF_TRADE_DAYS_IN_MONTH
-        return {"year":year_volatility,"month":month_volatility,"day":day_volatility}
+        three_week_volatility = year_volatility / self.SQUARE_ROOT_OF_TRADE_DAYS_IN_3WEEKS
+        return {"year":year_volatility,"month":month_volatility,"day":day_volatility,"3week":three_week_volatility}
 
