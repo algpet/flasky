1e1d9408b08feaafc92bfee5ce15fed25302d54f
algpet <litovec@gmail.com>
2017-08-20 10:39:11 +0300

    call option suggestions fixed

--- controllers/download_controller.py
+++ controllers/download_controller.py
@@ -3,6 +3,8 @@ from flask import send_file
 import shutil
 import openpyxl
 from openpyxl import load_workbook
+import time
+
 
 class DownloadController:
 
@@ -12,9 +14,10 @@ class DownloadController:
         pass
 
     def dispatch(self, request):
-        tickers, from_date, till_date = self.parameterService.init_params()
-        tickers = "aapl"
-        ticker_data = self.tickerRateService.get_rates(tickers, from_date, till_date)
+        tickers, from_date, till_date = self.parameterService.init_params(1500)
+        tickers = "goog"
+
+        ticker_data = self.tickerRateService.get_rate(tickers, from_date, till_date)
 
         dest_file = 'static/excel/excel_dummy2.xlsm'
 
@@ -23,15 +26,20 @@ class DownloadController:
         wb = load_workbook(filename=dest_file)
         ws = wb["Summary"]
         ws["b4"] = tickers
-        ws["b5"] = str(from_date)
-        ws["b6"] = str(till_date)
-        wb.save(dest_file)
+        ws["b5"] = from_date
+        ws["b6"] = till_date
 
-        return send_file(dest_file,
+        ws["d4"] = ticker_data.iloc[0]['Close']
+
+        #ws["d4"] = ticker_data[0]["Close"]
+        wb.save(dest_file)
+        print(time.time())
+        result = send_file(dest_file,
                          mimetype='text/csv',
                          attachment_filename='dummy.xlsm',
                          as_attachment=True)
-
+        print(time.time())
+        return result
 
 
 
