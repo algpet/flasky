93aee3d82eb635fea326420148b5576059099270
algpet <litovec@gmail.com>
2017-10-17 01:53:31 +0300

    bugfixes

--- controllers/profit_controller.py
+++ controllers/profit_controller.py
@@ -3,4 +3,4 @@ from controllers.dummy_controller import DummyController
 class ProfitController(DummyController):
     def __init__(self,template):
         DummyController.__init__(self,template)
-        super(ProfitController, self).__init__(template)
+        #super(ProfitController, self).__init__(template)
